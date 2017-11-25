6c642bedceaa66e9ffec75688528f9ae795e57f7
Quy <quy.nguyenngoctp@gmail.com>
2017-11-25 13:00:00 +0700

    add bulma

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+  def new 
+  end 
+
+  def create 
+    @user = User.new(user_params)
+    if @user.save 
+      redirect_to root_path
+    else 
+      redirect_to new_user_path
+    end 
+  end 
+
+  def user_params 
+    params.require(:user).permit(:first_name, :last_name)
+  end 
 end
