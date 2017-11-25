228f9dc4fe15bbabcdfcd33f4273a7ed7f28669b
Quy <quy.nguyenngoctp@gmail.com>
2017-11-25 13:11:41 +0700

    add basic div html to User's form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,10 @@ class UsersController < ApplicationController
   def create 
     @user = User.new(user_params)
     if @user.save 
+      flash[:success] = "Create new user successfully"
       redirect_to root_path
     else 
+      flash[:error] = "#{@user.errors.full_messages.to_sentence}"
       redirect_to new_user_path
     end 
   end 
