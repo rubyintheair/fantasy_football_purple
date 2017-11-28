8dbd96bba5e958e26ffc16cc78b1af4bf6b400d7
Quy <quy.nguyenngoctp@gmail.com>
2017-11-28 14:45:41 +0700

    unit test for teamplay model and relationship of teamplay && user

--- test/models/team_play_test.rb
+++ test/models/team_play_test.rb
@@ -4,4 +4,17 @@ class TeamPlayTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should have name, user and teammate" do 
+    a = TeamPlay.new 
+    assert_not a.save
+    assert_equal "Name can't be blank, User must exist, and Teammate must exist", a.errors.full_messages.to_sentence
+  end 
+
+  test "create successfully a TeamPlay from TeamPlay model" do 
+    a = TeamPlay.new(name: "123", user_id: User.first.id, teammate_id: User.last.id)
+    assert a.save 
+    assert_equal "123", TeamPlay.last.name
+    assert_includes TeamPlay.all, a 
+  end 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -35,25 +35,35 @@ class UserTest < ActiveSupport::TestCase
     assert_equal "First name can't be blank", a.errors.full_messages.to_sentence
   end 
 
-  test "add_friend should add a stranger user as a teammate" do 
-    a = User.new(first_name: "111", last_name: "2345")
-    assert a.save 
-    b = User.new(first_name: "222", last_name: "2345")
-    assert b.save
-    a.add_teammate(b)
-    assert_includes a.teammates, b 
-    assert_equal 1, a.teammates.count
-  end 
+  # test "add_friend should add a user as a teammate" do 
+  #   a = User.new(first_name: "111", last_name: "2345")
+  #   assert a.save 
+  #   b = User.new(first_name: "222", last_name: "2345")
+  #   assert b.save
+  #   a.add_teammate(b)
+  #   assert_includes a.teammates, b 
+  #   assert_equal 1, a.teammates.count
+  # end 
 
-  test "Create a teamplay as a user" do 
+  test "create successfully a TeamPlay from User model" do 
     a = User.first 
     b = User.last
     c = a.team_plays.new(teammate_id: b.id, name: "hahahaha")
     c.save
     assert c
+    assert_equal "hahahaha", c.name
   end 
 
-  
+  test "an user can find his/her teammate" do 
+    a = User.first 
+    b = User.last 
+    c = a.team_plays.new(teammate_id: b.id, name: "hahaha")
+    assert c.save 
+    assert_equal 1, a.teammates.count
+    assert_equal a.teammates.first.first_name, b.first_name 
+  end 
+
+
 
   
 
