9f89d572506bb9aeaf86e7d6a9c3542cf253fec9
Quy <quy.nguyenngoctp@gmail.com>
2017-11-28 22:18:40 +0700

    update Match Model

--- app/models/match.rb
+++ app/models/match.rb
@@ -4,20 +4,5 @@ class Match < ApplicationRecord
   has_many :games, dependent: :destroy
 #  has_many :line_results, dependent: :destroy
 
-  def owner_total_score #neu kieu line_result nhu Quy thi no chi la 1 phan tu chu dau phai 1 array
-    owner_score = line_results.inject(0.0) {|sum, e| sum + e.owner_score }
-  end 
-
-  def guest_total_score 
-    guest_score = line_results.inject(0.0) {|sum, e| sum + e.guest_score }
-  end 
-
-  def final_winner 
-    owner_total_score > guest_total_score ? owner_team : guest_team 
-  end 
-
-  def final_loser 
-    owner_total_score < guest_total_score ? owner_team : guest_team 
-  end 
-
+  
 end
