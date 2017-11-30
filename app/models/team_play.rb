741d6f02a4fd6362070e2b8c2e7ecdaba138c86a
Quy <quy.nguyenngoctp@gmail.com>
2017-11-30 08:25:56 +0700

    add navbar, navbar_item,... , resources of matches, game_items, team_plays in route

--- app/models/team_play.rb
+++ app/models/team_play.rb
@@ -4,6 +4,7 @@ class TeamPlay < ApplicationRecord
   belongs_to :teammate, class_name: "User" 
   has_many :teams, as: :team_display
 
+
   # has_many :owner_teams, foreign_key: "owner_team_id"    Useless code???
   # has_many :guest_teams, foreign_key: "guest_team_id"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
+  resources :matches 
+  resources :game_items
+  resources :team_plays
 
   root "home#index"
 end
