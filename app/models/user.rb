8a7a4c53bbb31a55317fc09d450df163d948373c
Quy <quy.nguyenngoctp@gmail.com>
2017-12-01 23:42:01 +0700

    add gem faker

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
   validates :first_name, :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
   has_many :team_plays, dependent: :destroy 
   has_many :teammates, through: :team_plays 
   has_many :single_play_news, dependent: :destroy
   has_many :teams, dependent: :destroy
+  has_secure_password
 
   def add_teammate(another_user)
     teammates << another_user
