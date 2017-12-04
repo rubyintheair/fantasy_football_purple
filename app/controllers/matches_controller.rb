cc35984004a77c124e67d8752ca72e17553f8447
Quy <quy.nguyenngoctp@gmail.com>
2017-12-04 10:51:46 +0700

    complete create Match

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,2 +1,26 @@
 class MatchesController < ApplicationController
+  def index 
+  end 
+
+  def new 
+    @owner_teams = TeamPlay.where(user_id: current_user.id)
+    @guest_teams = TeamPlay.where.not("user_id = ? AND teammate_id = ?", current_user.id, current_user.id)
+  end 
+
+  def create 
+    @match = Match.new(match_params)
+    @match.user_id = current_user.id 
+    if @match.save 
+      flash[:success] = "Create match successfully"
+      redirect_to matches_path
+    else 
+      flash[:error] = "#{@match.errors.full_messages.to_sentence}"
+      redirect_to new_match_path
+    end 
+  end 
+
+  def match_params
+    params.require(:match).permit(:owner_team_id, :guest_team_id)
+  end 
+
 end
--- app/models/match.rb
+++ app/models/match.rb
@@ -4,7 +4,7 @@ class Match < ApplicationRecord
   belongs_to :user
   has_many :games, dependent: :destroy
   validate :check_games
-  validate :check_team_member_match
+  # validate :check_team_member_match
   
 
   def check_games
@@ -13,11 +13,11 @@ class Match < ApplicationRecord
     end 
   end 
 
-  def check_team_member_match
-    if owner_team.user.id == guest_team.user.id || owner_team.user.id == guest_team.teammate.id || owner_team.teammate.id == guest_team.user.id || owner_team.teammate.id == guest_team.teammate.id
-      errors.add(:match, "can not have the same member in 2 team")
-    end 
-  end 
+  # def check_team_member_match
+  #   if owner_team.user.id == guest_team.user.id || owner_team.user.id == guest_team.teammate.id || owner_team.teammate.id == guest_team.user.id || owner_team.teammate.id == guest_team.teammate.id
+  #     errors.add(:match, "can not have the same member in 2 team")
+  #   end 
+  # end 
 
   
 #  has_many :line_results, dependent: :destroy
